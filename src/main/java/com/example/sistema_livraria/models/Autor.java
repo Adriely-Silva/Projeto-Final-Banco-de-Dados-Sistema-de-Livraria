e39e410310a812d1cb9e173513c583115b84c7b6
Adriely <adrielysilva010@gmail.com>
2025-02-05 15:45:46 -0300

    Retirando getters e setters

--- src/main/java/com/example/sistema_livraria/models/Autor.java
+++ src/main/java/com/example/sistema_livraria/models/Autor.java
@@ -20,20 +20,4 @@ public class Autor implements Serializable {
 
     @NotNull
     String nome;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
--- src/main/java/com/example/sistema_livraria/models/Editora.java
+++ src/main/java/com/example/sistema_livraria/models/Editora.java
@@ -17,20 +17,4 @@ public class Editora implements Serializable {
 
     @NotNull
     String nome;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
